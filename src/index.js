d7b473ae7f326983256f8074f777a74b1d4959f6
Compiler Exe <compilerexe@gmail.com>
2018-01-18 12:40:01 +0700

    add mockup design.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'jquery/dist/jquery.min'
+import 'popper.js/dist/umd/popper.min'
+import 'bootstrap/dist/js/bootstrap.min'
+
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
